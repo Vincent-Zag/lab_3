3b7313b853c5b969d17b7f6b0e2b899df6eb98dd
Vincent-Zag <acewinner11@gmail.com>
2020-02-05 12:48:59 -0700

    Added Problem Answers

---
+++ lab3_scripts.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+# Authors : Vincent Zagala
+# Date:  2/5/2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+#1.
+echo "Enter File Name: "
+read FileName
+echo "Enter Regular Expression: "
+read RegEx
+grep $RegEx $FileName
+grep -c $FileName
